6ab7f79ce1ceb06fc7f5c2ae0ddca524937b8dae
Kaedyn Bolin <kaedynbolin1@gmail.com>
2024-04-24 15:50:28 -0500

    Added code documentation

--- compound-interest-calculator.c
+++ compound-interest-calculator.c
@@ -2,18 +2,34 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * This function handles the calculations for the compound interest calculator. 
+ * See https://en.wikipedia.org/wiki/Compound_interest#Calculation for more details.
+ *
+ * Args: 
+ *  P - principal amount
+ *  r - nominal annual interest rate
+ *  n - compounding frequency 
+ *  t - length of time interest is applied (in years)
+ *
+ * Return: 
+ *  A - final compounded amount
+ */
 double compound_interest_calculator(double P, double r, double n, double t)
 {
     return P * pow((1 + (r / n)), n * t);
 }
 
+/**
+ * Main function.
+ */
 int main(int argc, char **argv)
 {
-    double P;
-    double r;
-    double n;
-    double t;
-    double A;
+    double A;   // final compounded amount
+    double P;   // principal amount
+    double r;   // nominal annual interest rate
+    double n;   // compounding frequency
+    double t;   // length of time interest is applied (in years)
 
     P = 1000;
     r = 1.43;
